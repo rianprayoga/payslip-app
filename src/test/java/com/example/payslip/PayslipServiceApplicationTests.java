ffb8d799d9c116160f0fe51c242ef6c7c023e219
rian <junianto.riansp@gmail.com>
2025-06-08 08:55:15 +0700

    chore: init

---
+++ src/test/java/com/example/payslip/PayslipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayslipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
